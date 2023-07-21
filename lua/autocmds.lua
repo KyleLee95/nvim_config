57c27ec43dca7bb543e7663df829a4a34f21bf04
Kyle <kyle@kylelee.dev>
2023-07-21 18:39:18 -0500

    stuff

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,13 +1,13 @@
 --trigger nabla on .wiki files
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	pattern = { "*.wiki" },
-	command = "echo 'Entering a wiki file'",
+	command = '',
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	pattern = { "*.wiki" },
 	callback = function()
-		print("here")
+		-- print("here")
 		require("nabla").enable_virt({
 			autogen = true, -- auto-regenerate ASCII art when exiting insert mode
 			silent = true, -- silents error messages
